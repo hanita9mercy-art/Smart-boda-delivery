f61ba799c072890752aa7834d81988876bf7b584
hanita9mercy-art <hanita9mercy@gmail.com>
2026-07-26 20:28:27 +0300

    Create rideRoutes.js

---
+++ rideRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const rideController = require('./rideController');
+
+// Routes
+router.post('/create', rideController.createRide);
+router.get('/available', rideController.getNearbyRides);
+router.patch('/accept', rideController.acceptRide);
+router.patch('/complete', rideController.completeRide);
+router.patch('/cancel', rideController.cancelRide);
+
+module.exports = router;
