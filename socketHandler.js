a2705bb7a7f7715d67e1e3a5b794c2aaca7630a1
hanita9mercy-art <hanita9mercy@gmail.com>
2026-07-26 20:35:53 +0300

    Update socketHandler.js

--- socketHandler.js
+++ socketHandler.js
@@ -2,10 +2,27 @@ module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('A user connected: ' + socket.id);
 
+        // 1. Customer joins a specific "ride room" to track the Boda
+        socket.on('joinRide', (rideId) => {
+            socket.join(`ride_${rideId}`);
+            console.log(`Socket ${socket.id} joined room: ride_${rideId}`);
+        });
+
+        // 2. Rider sends their location
+        socket.on('sendLocation', (data) => {
+            // data format: { rideId: '123', latitude: 0.123, longitude: 32.123 }
+            const { rideId, latitude, longitude } = data;
+            
+            // Send this location to everyone in that specific ride's room
+            io.to(`ride_${rideId}`).emit('locationUpdated', { 
+                rideId, 
+                latitude, 
+                longitude 
+            });
+        });
+
         socket.on('disconnect', () => {
             console.log('User disconnected');
         });
-
-        // Add more socket events here as your app grows
     });
 };
