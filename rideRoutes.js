c443c827e50bbb3dd26d7811bbc16198f37484fc
hanita9mercy-art <hanita9mercy@gmail.com>
2026-07-27 12:15:29 +0300

    Update rideRoutes.js

--- rideRoutes.js
+++ rideRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const rideController = require('./rideController');
 
-// Routes mapping
+// Import Controllers
+const rideController = require('../controllers/rideController');
+const paymentController = require('../controllers/paymentController');
+
+// Ride Routes
 router.post('/create', rideController.createRide);
 router.get('/available', rideController.getNearbyRides);
 router.patch('/accept', rideController.acceptRide);
 router.patch('/complete', rideController.completeRide);
 router.patch('/cancel', rideController.cancelRide);
+
+// Payment Routes
+router.post('/initiate-payment', paymentController.initiatePayment);
 
 module.exports = router;
